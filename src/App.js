9537a7099192eb93e98d6761a3cbe680606574ad
Deepika <bhuvanadeepika017@gmail.com>
2020-08-28 13:35:49 +0530

    added base converters app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class BaseConverter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    const value = event.target.value;
+    const lastNumber = value.split('').slice(-1);
+    const lastKey = parseInt(lastNumber, this.props.base);
+    if (isNaN(lastKey) && value.length !== 0) return;
+    this.props.onChange(parseInt(value, this.props.base));
+  }
+
+  render() {
+    const value = isNaN(this.props.value) ? '' : this.props.value;
+    return (
+      <div>
+        <label>
+          Base {this.props.base} :
+        <input
+            type='text'
+            value={value.toString(this.props.base)}
+            onChange={this.handleClick}></input>
+        </label>
+      </div>
+    );
+  }
+}
+
+class BaseConverters extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: 0 };
+    this.base = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(value) {
+    this.setState({ value });
+  }
+
+  render() {
+    const baseConverters = this.base.map((base) => (
+      <BaseConverter
+        base={base}
+        value={this.state.value}
+        onChange={this.handleChange}
+        key={base}
+      />
+    ));
+    return <div style={{ margin: '30px' }}>
+      <h2>Base Converters</h2>
+      {baseConverters}
+    </div>;
+  }
+}
+
+const App = () => {
+  return <BaseConverters />
 }
 
 export default App;
