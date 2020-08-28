53f950f11444d30628fecb56142be416d3320739
Deepika <bhuvanadeepika017@gmail.com>
2020-08-28 13:44:10 +0530

    Updates

---
+++ precache-manifest.6f1579340cba49d168d720779c001b74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9086149b89a9abfdba9fc235886767a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2740d4fca3ebf8c347c5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d9bdd1b291ebbfc0dfe8",
+    "url": "/static/js/2.01001a22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.01001a22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2740d4fca3ebf8c347c5",
+    "url": "/static/js/main.1bf60b6c.chunk.js"
+  },
+  {
+    "revision": "78dc9704994c91357ca1",
+    "url": "/static/js/runtime-main.62f72b60.js"
+  }
+]);
